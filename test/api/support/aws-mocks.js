dc19382aa3d4ab856ffb941ca981b6ab31bd20c6
Sven Aas <sven.aas@gsa.gov>
2023-08-14 10:40:06 -0400

    Comment out the content of aws-mocks.js (not deleting yet because it's widely required and we may still use it)

--- test/api/support/aws-mocks.js
+++ test/api/support/aws-mocks.js
@@ -1,43 +1,43 @@
-const AWS = require('aws-sdk');
+// const AWS = require('aws-sdk');
 
-const mocks = {
-  S3: {},
-  SQS: {},
-};
+// const mocks = {
+//   S3: {},
+//   SQS: {},
+// };
 
-const mockableFunctions = {
-  S3: [
-    'getObject',
-    'listObjectsV2',
-    'deleteObjects',
-    'putObject',
-    'headBucket',
-  ],
-  SQS: ['sendMessage'],
-};
+// const mockableFunctions = {
+//   S3: [
+//     'getObject',
+//     'listObjectsV2',
+//     'deleteObjects',
+//     'putObject',
+//     'headBucket',
+//   ],
+//   SQS: ['sendMessage'],
+// };
 
-Object.keys(mockableFunctions).forEach((service) => {
-  AWS[service] = function mock() {};
+// Object.keys(mockableFunctions).forEach((service) => {
+//   AWS[service] = function mock() {};
 
-  mockableFunctions[service].forEach((functionName) => {
-    // eslint-disable-next-line consistent-return
-    AWS[service].prototype[functionName] = (params, cb) => {
-      if (mocks[service][functionName]) {
-        if (!cb) {
-          return mocks[service][functionName](params);
-        }
-        mocks[service][functionName](params, cb);
-        // eslint-disable-next-line consistent-return
-        return;
-      }
-      cb(null, {});
-    };
-  });
-});
+//   mockableFunctions[service].forEach((functionName) => {
+//     // eslint-disable-next-line consistent-return
+//     AWS[service].prototype[functionName] = (params, cb) => {
+//       if (mocks[service][functionName]) {
+//         if (!cb) {
+//           return mocks[service][functionName](params);
+//         }
+//         mocks[service][functionName](params, cb);
+//         // eslint-disable-next-line consistent-return
+//         return;
+//       }
+//       cb(null, {});
+//     };
+//   });
+// });
 
-const resetMocks = () => {
-  mocks.S3 = {};
-  mocks.SQS = {};
-};
+// const resetMocks = () => {
+//   mocks.S3 = {};
+//   mocks.SQS = {};
+// };
 
-module.exports = { mocks, resetMocks };
+// module.exports = { mocks, resetMocks };
