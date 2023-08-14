ffb42d8ebb4f86712bca6e6f80186a117bf4e5e4
Drew Bollinger <drew.bollinger@gsa.gov>
2023-08-14 09:52:20 -0400

    test globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,15 @@ const overrides = [
       'node': true
     },
   },
+  {
+    files: [
+      "**/*.test.js"
+    ],
+    env: {
+      jest: true,
+    },
+    plugins: ["jest"],
+  }
 ];
 
 module.exports = {
